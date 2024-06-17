229e776681132c34d1d9c11b6bae1d3e24bdeffa
Troy <tydamon@hotmail.com>
2024-06-18 00:11:36 +1000

    making stockpile container event listener, upturned card appear when you click back-up card

--- client/src/components/StockPile.jsx
+++ client/src/components/StockPile.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import '../App.css'; // Stylesheet for the solitaire game
 import '../solitaire.css';
 
+
 const StockPile = () => {
+    const [boxClicked, setBoxClicked] = useState(false);
+
+    const handleClick = () => {
+      setBoxClicked(true);
+    };
+  
     return (
-        <div className="two-boxes-container">
-          <div className="box box1">
-            Box 1
-          </div>
-          <div className="box box2">
+      <div className="stockpile-container">
+        <div className="sp box1" onClick={handleClick}>
+          Box 1
+        </div>
+        {boxClicked && (
+          <div className="sp box2">
             Box 2
           </div>
-        </div>
-      );
-    }
+        )}
+      </div>
+    );
+  }
 
 export default StockPile;
