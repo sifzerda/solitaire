cb801f01c86d16fb25b86824898adeeb450e2d19
Troy <tydamon@hotmail.com>
2024-06-22 18:28:46 +1000

    ...

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,7 @@ import Notableau from '../components/Notableau';
 export default function About () {
   return (
     <div>
-      <h1>About Me</h1>
+      <h1>Solitaire</h1>
 
       <p className="separator-line"></p>
 
@@ -17,13 +17,7 @@ export default function About () {
       <div className="separator-line"></div>
 
       <p className='portfolio-bio'>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.</p>
  
 
  <Notableau />
