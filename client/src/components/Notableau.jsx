daf4f5ae5959a3e61bbb6d2dc7259decb9688257
Troy <tydamon@hotmail.com>
2024-06-24 00:54:27 +1000

    added card color to card display, this is easily removed via initialCards and return - {card.color}

--- client/src/components/Notableau.jsx
+++ client/src/components/Notableau.jsx
@@ -4,64 +4,64 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 // Define initial squares and boxes data
 const initialCards = [
   // HEARTS 
-  { id: 'card-1', suit: 'Hearts', rank: 'Ace' },
-  { id: 'card-2', suit: 'Hearts', rank: '2' },
-  { id: 'card-3', suit: 'Hearts', rank: '3' },
-  { id: 'card-4', suit: 'Hearts', rank: '4' },
-  { id: 'card-5', suit: 'Hearts', rank: '5' },
-  { id: 'card-6', suit: 'Hearts', rank: '6' },
-  { id: 'card-7', suit: 'Hearts', rank: '7' },
-  { id: 'card-8', suit: 'Hearts', rank: '8' },
-  { id: 'card-9', suit: 'Hearts', rank: '9' },
-  { id: 'card-10', suit: 'Hearts', rank: '10' },
-  { id: 'card-11', suit: 'Hearts', rank: 'Jack' },
-  { id: 'card-12', suit: 'Hearts', rank: 'Queen' },
-  { id: 'card-13', suit: 'Hearts', rank: 'King' },
+  { id: 'card-1', suit: 'Hearts', rank: 'Ace', color: 'Red' },
+  { id: 'card-2', suit: 'Hearts', rank: '2', color: 'Red' },
+  { id: 'card-3', suit: 'Hearts', rank: '3', color: 'Red' },
+  { id: 'card-4', suit: 'Hearts', rank: '4', color: 'Red' },
+  { id: 'card-5', suit: 'Hearts', rank: '5', color: 'Red' },
+  { id: 'card-6', suit: 'Hearts', rank: '6', color: 'Red' },
+  { id: 'card-7', suit: 'Hearts', rank: '7', color: 'Red' },
+  { id: 'card-8', suit: 'Hearts', rank: '8', color: 'Red' },
+  { id: 'card-9', suit: 'Hearts', rank: '9', color: 'Red' },
+  { id: 'card-10', suit: 'Hearts', rank: '10', color: 'Red' },
+  { id: 'card-11', suit: 'Hearts', rank: 'Jack', color: 'Red' },
+  { id: 'card-12', suit: 'Hearts', rank: 'Queen', color: 'Red' },
+  { id: 'card-13', suit: 'Hearts', rank: 'King', color: 'Red' },
 
   // DIAMONDS
-  { id: 'card-14', suit: 'Diamonds', rank: 'Ace' },
-  { id: 'card-15', suit: 'Diamonds', rank: '2' },
-  { id: 'card-16', suit: 'Diamonds', rank: '3' },
-  { id: 'card-17', suit: 'Diamonds', rank: '4' },
-  { id: 'card-18', suit: 'Diamonds', rank: '5' },
-  { id: 'card-19', suit: 'Diamonds', rank: '6' },
-  { id: 'card-20', suit: 'Diamonds', rank: '7' },
-  { id: 'card-21', suit: 'Diamonds', rank: '8' },
-  { id: 'card-22', suit: 'Diamonds', rank: '9' },
-  { id: 'card-23', suit: 'Diamonds', rank: '10' },
-  { id: 'card-24', suit: 'Diamonds', rank: 'Jack' },
-  { id: 'card-25', suit: 'Diamonds', rank: 'Queen' },
-  { id: 'card-26', suit: 'Diamonds', rank: 'King' },
+  { id: 'card-14', suit: 'Diamonds', rank: 'Ace', color: 'Red' },
+  { id: 'card-15', suit: 'Diamonds', rank: '2', color: 'Red' },
+  { id: 'card-16', suit: 'Diamonds', rank: '3', color: 'Red' },
+  { id: 'card-17', suit: 'Diamonds', rank: '4', color: 'Red' },
+  { id: 'card-18', suit: 'Diamonds', rank: '5', color: 'Red' },
+  { id: 'card-19', suit: 'Diamonds', rank: '6', color: 'Red' },
+  { id: 'card-20', suit: 'Diamonds', rank: '7', color: 'Red' },
+  { id: 'card-21', suit: 'Diamonds', rank: '8', color: 'Red' },
+  { id: 'card-22', suit: 'Diamonds', rank: '9', color: 'Red' },
+  { id: 'card-23', suit: 'Diamonds', rank: '10', color: 'Red' },
+  { id: 'card-24', suit: 'Diamonds', rank: 'Jack', color: 'Red' },
+  { id: 'card-25', suit: 'Diamonds', rank: 'Queen', color: 'Red' },
+  { id: 'card-26', suit: 'Diamonds', rank: 'King', color: 'Red' },
 
   // CLUBS
-  { id: 'card-27', suit: 'Clubs', rank: 'Ace' },
-  { id: 'card-28', suit: 'Clubs', rank: '2' },
-  { id: 'card-29', suit: 'Clubs', rank: '3' },
-  { id: 'card-30', suit: 'Clubs', rank: '4' },
-  { id: 'card-31', suit: 'Clubs', rank: '5' },
-  { id: 'card-32', suit: 'Clubs', rank: '6' },
-  { id: 'card-33', suit: 'Clubs', rank: '7' },
-  { id: 'card-34', suit: 'Clubs', rank: '8' },
-  { id: 'card-35', suit: 'Clubs', rank: '9' },
-  { id: 'card-36', suit: 'Clubs', rank: '10' },
-  { id: 'card-37', suit: 'Clubs', rank: 'Jack' },
-  { id: 'card-38', suit: 'Clubs', rank: 'Queen' },
-  { id: 'card-39', suit: 'Clubs', rank: 'King' },
+  { id: 'card-27', suit: 'Clubs', rank: 'Ace', color: 'Black' },
+  { id: 'card-28', suit: 'Clubs', rank: '2', color: 'Black' },
+  { id: 'card-29', suit: 'Clubs', rank: '3', color: 'Black' },
+  { id: 'card-30', suit: 'Clubs', rank: '4', color: 'Black' },
+  { id: 'card-31', suit: 'Clubs', rank: '5', color: 'Black' },
+  { id: 'card-32', suit: 'Clubs', rank: '6', color: 'Black' },
+  { id: 'card-33', suit: 'Clubs', rank: '7', color: 'Black' },
+  { id: 'card-34', suit: 'Clubs', rank: '8', color: 'Black' },
+  { id: 'card-35', suit: 'Clubs', rank: '9', color: 'Black' },
+  { id: 'card-36', suit: 'Clubs', rank: '10', color: 'Black' },
+  { id: 'card-37', suit: 'Clubs', rank: 'Jack', color: 'Black' },
+  { id: 'card-38', suit: 'Clubs', rank: 'Queen', color: 'Black' },
+  { id: 'card-39', suit: 'Clubs', rank: 'King', color: 'Black' },
 
   // SPADES
-  { id: 'card-40', suit: 'Spades', rank: 'Ace' },
-  { id: 'card-41', suit: 'Spades', rank: '2' },
-  { id: 'card-42', suit: 'Spades', rank: '3' },
-  { id: 'card-43', suit: 'Spades', rank: '4' },
-  { id: 'card-44', suit: 'Spades', rank: '5' },
-  { id: 'card-45', suit: 'Spades', rank: '6' },
-  { id: 'card-46', suit: 'Spades', rank: '7' },
-  { id: 'card-47', suit: 'Spades', rank: '8' },
-  { id: 'card-48', suit: 'Spades', rank: '9' },
-  { id: 'card-49', suit: 'Spades', rank: '10' },
-  { id: 'card-50', suit: 'Spades', rank: 'Jack' },
-  { id: 'card-51', suit: 'Spades', rank: 'Queen' },
-  { id: 'card-52', suit: 'Spades', rank: 'King' },
+  { id: 'card-40', suit: 'Spades', rank: 'Ace', color: 'Black' },
+  { id: 'card-41', suit: 'Spades', rank: '2', color: 'Black' },
+  { id: 'card-42', suit: 'Spades', rank: '3', color: 'Black' },
+  { id: 'card-43', suit: 'Spades', rank: '4', color: 'Black' },
+  { id: 'card-44', suit: 'Spades', rank: '5', color: 'Black' },
+  { id: 'card-45', suit: 'Spades', rank: '6', color: 'Black' },
+  { id: 'card-46', suit: 'Spades', rank: '7', color: 'Black' },
+  { id: 'card-47', suit: 'Spades', rank: '8', color: 'Black' },
+  { id: 'card-48', suit: 'Spades', rank: '9', color: 'Black' },
+  { id: 'card-49', suit: 'Spades', rank: '10', color: 'Black' },
+  { id: 'card-50', suit: 'Spades', rank: 'Jack', color: 'Black' },
+  { id: 'card-51', suit: 'Spades', rank: 'Queen', color: 'Black' },
+  { id: 'card-52', suit: 'Spades', rank: 'King', color: 'Black' },
 ];
 
 // create and initialize Foundation decks with suit id, and empty 
@@ -241,7 +241,7 @@ return (
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}
                       >
-                        {cards[currentCardIndex].rank} of {cards[currentCardIndex].suit}
+                        {cards[currentCardIndex].rank} of {cards[currentCardIndex].suit} - {cards[currentCardIndex].color}
                       </div>
                     )}
                   </Draggable>
@@ -270,7 +270,7 @@ return (
                       ) : (
                         deck.cards.map((card, index) => (
                           <div key={card.id} className="card-in-deck">
-                            {card.rank} of {card.suit}
+                            {card.rank} of {card.suit} - {card.color}
                           </div>
                         ))
                       )}
@@ -305,7 +305,7 @@ return (
                               {...provided.dragHandleProps}
                               ref={provided.innerRef}
                             >
-                              {card.rank} of {card.suit}
+                              {card.rank} of {card.suit} - {card.color}
                             </div>
                           )}
                         </Draggable>
