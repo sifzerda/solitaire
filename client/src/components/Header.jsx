ec49c34da835d2abb39b0cfc75a3bd356463af61
Troy <tydamon@hotmail.com>
2024-06-25 23:20:11 +1000

    reformatted header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,14 @@ const Header = () => {
   return (
     <header className='header'>
       <header className='header-gold'>
+      <span className='spades'>♤</span>
         <h1 className="title-with-border">
           <span className="diamond-border">
             <span className='diamond'>♢</span>
-            <span className='clubs'>♧</span>
             <span className="solitaire-text">solitaire</span>
             <span className='hearts'>♡</span>
-            <span className='spades'>♤</span>
           </span>
+          <span className='clubs'>♧</span>
         </h1>
       </header>
     </header>
