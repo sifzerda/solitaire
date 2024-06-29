8ad24b8ff4a2a063bdca7eb06eb5110c0e83612a
Troy <tydamon@hotmail.com>
2024-06-30 08:26:53 +1000

    ...

--- client/src/components/FinalScore.jsx
+++ client/src/components/FinalScore.jsx
@@ -1,7 +1,7 @@
 const FinalScore = ({ timer }) => {
     return (
       <div className='final-container'>
-        <h1>Game Completed!</h1>
+        <h1>game completed!</h1>
         <p className='timer'>Your time was: {timer}</p>
       </div>
     );
